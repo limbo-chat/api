c6838882280607b9a4a1948f162e1796758931b2
Wes Lowe <wesley.lowe@yahoo.com>
2025-04-11 14:51:08 -0500

    restructured plugin api and added discriminated settings types

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,87 @@
 export interface Notification {
+	type: "info" | "error" | "warning";
 	message: string;
 }
 
-export interface Setting {
+export interface BaseSetting {
 	id: string;
 	title: string;
 	helpText?: string;
-	type: "text" | "number";
-	defaultValue: string | number;
 }
+
+export interface TextSetting extends BaseSetting {
+	type: "text";
+	defaultValue?: string;
+	/**
+	 * The type of text input to display
+	 *
+	 * string: single line text input
+	 * password: single line text input with protected text
+	 *
+	 * @default string
+	 */
+	variant?: "string" | "password";
+}
+
+export interface BooleanSetting extends BaseSetting {
+	type: "boolean";
+
+	/**
+	 * @default false
+	 */
+	defaultValue?: boolean;
+}
+
+export type Setting = TextSetting | BooleanSetting;
 
 export interface LLM {
 	id: string;
 	title: string;
 	description: string;
-	// generateText: () => Promise<string>
+	generateText: () => Promise<string>;
 }
 
-export interface ToolbarToggle {
-	id: string;
-	label: string;
-	tooltip?: string;
+export declare namespace settings {
+	/**
+	 * Registers a setting that can be configured by the user
+	 */
+	export function register(setting: Setting): void;
+
+	/**
+	 * Unregisters a setting that can be configured by the user
+	 */
+	export function unregister(settingId: string): void;
+
+	/**
+	 * Gets a setting value for the plugin
+	 */
+	export function get<T extends string | boolean = string | boolean>(
+		settingId: string
+	): T | undefined;
 }
 
-export declare function showNotification(notification: Notification): void;
-export declare function registerSetting(setting: Setting): void;
-export declare function unregisterSetting(settingId: string): void;
-export declare function registerLLM(llm: LLM): void;
-export declare function unregisterLLM(llmId: string): void;
-export declare function registerToolbarToggle(toolbarToggle: ToolbarToggle): void;
-export declare function unregisterToolbarToggle(toolbarToggleId: string): void;
+export declare namespace notifications {
+	/**
+	 * Shows a notification to the user
+	 */
+
+	export function show(notification: Notification): void;
+}
+
+export declare namespace llms {
+	/**
+	 * Registers a new LLM that can be used
+	 */
+	export function register(llm: LLM): void;
+
+	/**
+	 * Unregisters an llm from the app
+	 */
+	export function unregister(llmId: string): void;
+}
 
 export interface API {
-	showNotification: typeof showNotification;
-	registerSetting: typeof registerSetting;
-	unregisterSetting: typeof unregisterSetting;
-	registerLLM: typeof registerLLM;
-	unregisterLLM: typeof unregisterLLM;
-	registerToolbarToggle: typeof registerToolbarToggle;
-	unregisterToolbarToggle: typeof unregisterToolbarToggle;
+	settings: typeof settings;
+	notifications: typeof notifications;
+	llms: typeof llms;
 }
-
-export default {
-	showNotification,
-	registerSetting,
-	unregisterSetting,
-	registerLLM,
-	unregisterLLM,
-	registerToolbarToggle,
-	unregisterToolbarToggle,
-};
