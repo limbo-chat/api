103d5009ec884759369c21879106776e924ad753
Wes Lowe <wesley.lowe@yahoo.com>
2025-05-21 12:01:19 -0500

    added removing items and clearing the storage

--- src/index.ts
+++ src/index.ts
@@ -141,6 +141,8 @@ export declare namespace notifications {
 export declare namespace storage {
 	export function set(key: string, value: any): void;
 	export function get<T>(key: string): T | undefined;
+	export function remove<T>(key: string): void;
+	export function clear(): void;
 }
 
 export declare namespace models {
