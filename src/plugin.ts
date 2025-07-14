61f675d99f265ebb737fa40b2d40407d3ecdb4b8
Wes Lowe <wesley.lowe@yahoo.com>
2025-07-13 21:03:38 -0500

    added doc comments to the plugin interface

--- src/plugin.ts
+++ src/plugin.ts
@@ -15,11 +15,24 @@ export interface OnTransformChatPromptArgs {
 }
 
 export interface Plugin {
+	/** Called when the plugin is activated */
 	onActivate?(): void | Promise<void>;
+
+	/** Called when the plugin is deactivated */
 	onDeactivate?(): void | Promise<void>;
+
+	/** Called when a chat is created */
 	onChatCreated?(args: OnChatCreatedArgs): void | Promise<void>;
+
+	/** Called when a chat is deleted */
 	onChatDeleted?(chatId: string): void | Promise<void>;
+
+	/** Called when multiple chats are deleted */
 	onChatsDeleted?(chatIds: string[]): void | Promise<void>;
+
+	/** Called once at the beginning of a chat loop */
 	onPrepareChatPrompt?(args: OnPrepareChatPromptArgs): void | Promise<void>;
+
+	/** Called before each iteration of a chat loop */
 	onTransformChatPrompt?(args: OnTransformChatPromptArgs): void | Promise<void>;
 }
